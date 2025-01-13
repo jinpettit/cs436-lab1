f363107c41e0a68ec99e977e6284cb86f61b3a97
jinpettit <jinpettit@gmail.com>
2025-01-13 04:21:56 -0800

    2.1

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zybooks.graduationrsvp
 
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +10,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +36,11 @@ class MainActivity : ComponentActivity() {
 fun GradScreen(modifier: Modifier = Modifier) {
     Text(
         text = "Graduation Announcement",
+        fontSize = 50.sp,
+        lineHeight = 50.sp,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
         modifier = modifier
     )
 }
