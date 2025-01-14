a2db85753b3073b7e12fc7158c802de170e62b4e
jinpettit <jinpettit@gmail.com>
2025-01-13 19:23:40 -0800

    2.3

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,13 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
+    )
     Text(
         text = stringResource(R.string.graduation_announcement),
         fontSize = 50.sp,
-        lineHeight = 50.sp,
         color = Color.Red,
-        fontWeight = FontWeight.Bold,
         textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 50.sp,
         modifier = modifier
     )
 }
